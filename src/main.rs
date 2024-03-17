9f971dd30788f2fc8d596c982bda2b6da9264441
Rafael <rafaelcnunes@icloud.com>
2024-03-16 22:54:09 -0300

    Improve user input validation and output formatting
    - Implement loop to ensure user selects a valid conversion option, enhancing program robustness.
    - Integrate colored text and emojis for a more interactive and user-friendly terminal output.
    - Modify number output to display a maximum of two decimal places for clearer and more concise results.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,60 @@
+use std::io;
+use colored::*;
+
 mod converters;
 
 use converters::meters_to_kilometers;
 use converters::pounds_to_kilograms;
 use converters::inches_to_centimeters;
-use std::io;
 
 fn main() {
-    println!("Select a conversion:");
-    println!("1: Meters to Kilometers");
-    println!("2: Pounds to Kilograms");
-    println!("3: Inches to Centimeters");
+    let mut option = 0;
 
-    let mut option = String::new();
-    io::stdin().read_line(&mut option).expect("Failed to read input");
-    let option: u32 = match option.trim().parse() {
+    while option == 0 {
+        println!("{}", "Select a conversion:".blue().bold());
+        println!("1️: Meters to Kilometers");
+        println!("2️: Pounds to Kilograms");
+        println!("3️: Inches to Centimeters");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+
+        match input.trim().parse::<u32>() {
+            Ok(num) if num >= 1 && num <= 3 => {
+                option = num;
+            }
+            Ok(_) => println!("{}", "Please select a valid option from the list.".red()),
+            Err(_) => println!("{}", "Please enter a number.".red()),
+        }
+    }
+
+    println!("{}", "Enter the value for conversion:".green().bold());
+    let mut value = String::new();
+    io::stdin().read_line(&mut value).expect("Failed to read input");
+    let number: f64 = match value.trim().parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("Please enter a number for the selection.");
+            println!("{}", "Please enter a valid number.".red());
             return;
         }
     };
 
-    let mut value = String::new();
-    let mut valid_number = false;
-    let mut number = 0.0;
-
-    while !valid_number {
-        println!("Enter the value for conversion:");
-        value.clear();
-        io::stdin().read_line(&mut value).expect("Failed to read input");
-
-        match value.trim().parse::<f64>() {
-            Ok(num) => {
-                number = num;
-                valid_number = true;
-            }
-            Err(_) => {
-                println!("Please enter a valid number.");
-            }
-        }
-    }
-
     match option {
-        1 => println!("Result: {} kilometers", meters_to_kilometers::convert(number)),
-        2 => println!("Result: {} kilograms", pounds_to_kilograms::convert(number)),
-        3 => println!("Result: {} centimeters", inches_to_centimeters::convert(number)),
-        _ => println!("Invalid option!"),
+        1 =>
+            println!(
+                "Result: {:.2} kilometers",
+                meters_to_kilometers::convert(number).to_string().green()
+            ),
+        2 =>
+            println!(
+                "Result: {:.2} kilograms",
+                pounds_to_kilograms::convert(number).to_string().green()
+            ),
+        3 =>
+            println!(
+                "Result: {:.2} centimeters",
+                inches_to_centimeters::convert(number).to_string().green()
+            ),
+        _ => unreachable!(),
     }
 }
